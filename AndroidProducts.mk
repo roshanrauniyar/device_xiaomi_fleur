9913a622bcf7721932a37941042765a4fa73a7e2
RohitTiwari <rohitttiwari52@gmail.com>
2023-10-21 23:23:26 +0530

    Fleur: Import Makefiles For PixelExperience-Plus
    Signed-off-by: RohitTiwari <rohitttiwari52@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -7,7 +7,9 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/superior_fleur.mk \
     $(LOCAL_DIR)/evolution_fleur.mk \
-    $(LOCAL_DIR)/lineage_fleur.mk
+    $(LOCAL_DIR)/lineage_fleur.mk \
+    $(LOCAL_DIR)/aosp_fleur.mk
+
 COMMON_LUNCH_CHOICES := \
     superior_fleur-user \
     superior_fleur-userdebug \
@@ -17,4 +19,7 @@ COMMON_LUNCH_CHOICES := \
     evolution_fleur-eng \
     lineage_fleur-user \
     lineage_fleur-userdebug \
-    lineage_fleur-eng
+    lineage_fleur-eng \
+    aosp_fleur-user \
+    aosp_fleur-userdebug \
+    aosp_fleur-eng
