65f5297de2713399164b74b2862fcf058aab9fa7
Roshan Kumar Rauniyar <139573813+roshanrauniyar@users.noreply.github.com>
2023-11-05 19:35:07 +0545

    fleur: spark bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,27 +5,10 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aicp_fleur.mk 
-    #$(LOCAL_DIR)/evolution_fleur.mk 
-    #$(LOCAL_DIR)/lineage_fleur.mk 
-    #$(LOCAL_DIR)/aosp_fleur.mk
-    #$(LOCAL_DIR)/superior_fleur.mk 
+    $(LOCAL_DIR)/spark_fleur.mk
 
 COMMON_LUNCH_CHOICES := \
-    aicp_fleur-user \
-    aicp_fleur-userdebug \
-    aicp_fleur-eng 
-
-    #evolution_fleur-user \
-    #evolution_fleur-userdebug \
-    #evolution_fleur-eng 
-    #lineage_fleur-user \
-    #lineage_fleur-userdebug \
-    #lineage_fleur-eng 
-    #aosp_fleur-user \
-    #aosp_fleur-userdebug \
-    #aosp_fleur-eng
-    #superior_fleur-user \
-    #superior_fleur-userdebug \
-    #superior_fleur-eng 
+    spark_fleur-user \
+    spark_fleur-userdebug \
+    spark_fleur-eng  
     
